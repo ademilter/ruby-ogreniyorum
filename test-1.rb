08eff2fd3d7caab4adc62f708a4961420b0b41ff
Adem ilter <ademilter@gmail.com>
2015-12-26 20:44:49 +0200

    test-1

---
+++ test-1.rb
@@ -0,0 +1,32 @@
+# elimizde bir değişken var. bu değişkenin türü/tipi'ni nasıl
+# anlarız?
+# --------------------------------------------------------
+
+
+
+
+# aşağıdaki String'i upper-case'e dönüştürmek için
+# ne yapmak lazım ?
+# --------------------------------------------------------
+user_fullname = "uğur özyılmazel"
+
+
+
+
+# numbers Array'ine 3 tane sayı ekle. bunlar: 10,20,30
+# --------------------------------------------------------
+numbers = [1, 2, 3, 4, 5]
+
+
+
+
+# String'e ait method'ların ilk'ini göster
+# --------------------------------------------------------
+
+
+
+
+# b'nin değeri ne olabilir?
+# --------------------------------------------------------
+a = "vigo"
+b = a * 5
